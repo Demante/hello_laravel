c0c3c9efb3779064070c06db6b5493f57bc04b88
Demante <1239515290@qq.com>
2018-10-30 16:54:30 +0800

    update  10/30

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>@yield('title','Sample App')  - Laravel 新手入门教程</title>
+	<link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+	@include('layouts._header')
+	<div class="container">
+		<div class="col-mod-offset-1 col-md-10"></div>
+		@yield('content')
+		@include('layouts._footer')
+	</div>
+</body>
+</html>
