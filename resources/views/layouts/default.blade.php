aba1d356843b57be36ac3490a28592075b16193f
Demante <1239515290@qq.com>
2018-10-31 03:37:45 +0000

    Finish login and logout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>@yield('title','Sample App')  - Laravel 新手入门教程</title>
-	<link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
-	@include('layouts._header')
-	<div class="container">
-		<div class="col-mod-offset-1 col-md-10">
-			@include('shared._messges')
-			@yield('content')
-			@include('layouts._footer')
-		</div>
-	</div>
-</body>
+<html>
+  <head>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @include('shared._messges')
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
+
+    <script src="/js/app.js"></script>
+  </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,7 +22,7 @@ Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 // login
 Route::get('login', 'SessionsController@create')->name('login');  //登录
 Route::post('login', 'SessionsController@store')->name('login');  //创建新的会话
-Route::delete('login', 'SessionsController@destroy')->name('login');  //销毁会话
+Route::delete('logout', 'SessionsController@destroy')->name('logout');  //销毁会话
 // Route::get('/', function () {
 //     return view('welcome');
 // });
